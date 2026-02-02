f71bd96f2a6af25d4bc40f5f3cae764d343cf57d
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2026-02-02 21:14:01 +0530

    added visual.py

---
+++ pages/visual.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import plotly.express as fancyplot
+import pandas as pd
+import os
+
+
+movieset = pd.read_csv("../dataset.csv")
+
+genres = movieset["Genre"].drop_duplicates()
+
+
+if st.selectbox("Select Genre",genres,key="genre_type"):
+    genre_type = st.session_state.get("genre_type")
+
+    comedy = movieset[movieset["Genre"] == genre_type]
+
+    top_10 = comedy.sort_values(by=["Rating","Metascore","Votes"],ascending=False).head(10)
+
+    bar = fancyplot.bar(data_frame=top_10,x="Title",y="Rating")
+
+    st.subheader(f"Top 10 {genre_type} Movies by Rating")
+
+    st.dataframe(top_10[["Rating", "Title"]])
+
+    st.plotly_chart(bar)
+
+    total_votes = top_10["Votes"].count()
+
+    st.subheader(f"Top 10 {genre_type} Ordered by Votes")
+
+    chart = fancyplot.pie(top_10,top_10["Title"],top_10["Votes"])
+
+    st.plotly_chart(chart)
+
+
+
+
+
