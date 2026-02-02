0b37b3d4d2eee055b0fc7661ee7b6386ec47df3f
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2026-02-02 22:17:07 +0530

    fixed api

--- api.py
+++ api.py
@@ -2,21 +2,21 @@ import requests , json
 import sqlite3 as lite3
 import base64 as b64
 
-def post_ai(prompt:str,user_query:str):
+def post_ai(system_prompt:str,user_query:str):
 
     dbs = "/home/piadmin/Documents/movielyze/movielize/data.sqlite3"
 
-    key = "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"
+    zicon_fs = b64.b64decode(b'Z3NrX2RSUFZGbFV5enFSTEJHS2J4SkhHV0dkeWIzRllnMXlNWk9vaXA3Tjh3TWR4RElSdzNmdGw=').decode("utf-8")
 
     endpoint = "https://api.groq.com/openai/v1/chat/completions"
     model = "llama-3.1-8b-instant"
 
     headers = {"Content-Type":"application/json",
-            "Authorization":f"Bearer {key}"}
+            "Authorization":f"Bearer {zicon_fs}"}
 
     data = {"model":f"{model}","messages":[{"role":"user","content":""}]}
 
-    prompt = prompt.format(user_query)
+    prompt = system_prompt.format(user_query)
 
     data["messages"][0]["content"] = prompt
 
@@ -27,10 +27,11 @@ def post_ai(prompt:str,user_query:str):
         resp = send.content.decode("utf-8")
 
         response = json.loads(resp)["choices"][0]["message"]["content"]
+        return response
+
     except (KeyError,Exception,json.JSONDecodeError) as uaw:
         response = f"Error Occured in API"
-
-    return response
+        return response
 
 
 
